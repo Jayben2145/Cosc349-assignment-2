1a499885e4cf96f5cae4865e874f17ab82a8444d
tohja623 <tohja623@student.otago.ac.nz>
2024-09-29 09:11:46 +0000

    added a new password handling part for cognito

--- routes/login.js
+++ routes/login.js
@@ -3,13 +3,12 @@ const router = express.Router();
 const cognito = require('../config/cognito');
 
 // Render login page
-router.get('/', (req, res) => {
-  console.log('Login page hit!');
+router.get('/login', (req, res) => {
   res.render('login');
 });
 
 // Handle login POST request with Cognito
-router.post('/', async (req, res) => {
+router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   const params = {
@@ -17,30 +16,53 @@ router.post('/', async (req, res) => {
     ClientId: process.env.COGNITO_CLIENT_ID,
     AuthParameters: {
       USERNAME: username,
-      PASSWORD: password
-    }
+      PASSWORD: password,
+    },
   };
 
   try {
-    // Attempt to authenticate with Cognito
+    // Authenticate with Cognito
     const data = await cognito.initiateAuth(params).promise();
 
-    // Check if the IdToken is present in the response
-    if (!data || !data.AuthenticationResult || !data.AuthenticationResult.IdToken) {
-      console.error('Authentication failed: Missing IdToken');
-      return res.render('login', { error: 'Failed to authenticate: Missing IdToken.' });
+    // Check for NEW_PASSWORD_REQUIRED challenge
+    if (data.ChallengeName === 'NEW_PASSWORD_REQUIRED') {
+      req.session.challengeSession = data.Session;
+      req.session.username = username;
+      res.render('newPassword', { username });
+    } else {
+      const idToken = data.AuthenticationResult.IdToken;
+      req.session.isAuthenticated = true;
+      req.session.idToken = idToken;
+      res.redirect('/admin');
     }
-
-    const idToken = data.AuthenticationResult.IdToken;
-
-    // Store the token in the session for authentication
-    req.session.isAuthenticated = true;
-    req.session.idToken = idToken;
-
-    res.redirect('/admin');
   } catch (error) {
     console.error('Error authenticating:', error);
     res.render('login', { error: 'Invalid username or password' });
+  }
+});
+
+// Handle setting new password
+router.post('/new-password', async (req, res) => {
+  const { newPassword } = req.body;
+
+  const params = {
+    ChallengeName: 'NEW_PASSWORD_REQUIRED',
+    ClientId: process.env.COGNITO_CLIENT_ID,
+    ChallengeResponses: {
+      USERNAME: req.session.username,
+      NEW_PASSWORD: newPassword,
+    },
+    Session: req.session.challengeSession,
+  };
+
+  try {
+    const data = await cognito.respondToAuthChallenge(params).promise();
+    req.session.isAuthenticated = true;
+    req.session.idToken = data.AuthenticationResult.IdToken;
+    res.redirect('/admin');
+  } catch (error) {
+    console.error('Error setting new password:', error);
+    res.render('newPassword', { error: 'Could not set new password' });
   }
 });
 
